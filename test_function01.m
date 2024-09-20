c2d8ce6b712679d4e32b56bf938acb8778e10083
lilydao11 <ldao@olin.edu>
2024-09-20 11:22:09 -0400

    updated test_function01

--- test_function01.m
+++ test_function01.m
@@ -3,8 +3,8 @@ function [f_val, J] = test_function01(X)
         X(1)*X(3) + X(2) - 12;
         sin(X(1) + X(2) + X(3))];
 
-    J = [2*X(1) 2*X(2) -5*X(3);
+    J = [2*X(1) 2*X(2) -5*X(3)^4;
         X(3) 1 X(1);
-        cos(X(1) + X(2) + X(3)) cos(X(1) + X(2) + X(3)) cos(X(1) + X(2) + X(3));
+        cos(X(1) + X(2) + X(3)) cos(X(1) + X(2) + X(3)) cos(X(1) + X(2) + X(3))
         ];
 end
