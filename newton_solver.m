15e3804c0be6d92a30cf8162d1aae7da8c17c267
Ethan C <36670680+etbikehome@users.noreply.github.com>
2024-09-21 14:11:38 -0400

    Newton Solver

--- newton_solver.m
+++ newton_solver.m
@@ -1,11 +1,33 @@
-function x_next = newton_solver(fun, x0)
-%Loop through until x is small
-[f_val, J] = fun(x0);
-x_next = x0;
-    while abs(sum(f_val))> 1e-14 && abs(det(J*J')) > 1e-14
-        %calculate next x
-        x_next = x_next - J\f_val;
-        %establish next y value
-        [f_val, J]  = fun(x_next);        
+function x_next = multi_newton_solver(fun,x_guess,varargin)
+%true if supposed to use analytical jacobian, false otherwise
+use_analytical_jacobian = nargin==3 && varargin{1}(1);
+
+A_thresh = 10e-14;
+B_thresh = 10e-14;
+
+    if use_analytical_jacobian == true
+    f_val = fun(x_guess);
+    J = approximate_jacobian(fun,x_guess);
+    x_next = x_guess;
+        while abs(J\f_val)> A_thresh && abs(f_val)>B_thresh && abs(det(J*J')) > 1e-14
+            %calculate next x
+            x_next = x_next - J\f_val;
+            %establish next y value
+            f_val = fun(x_next);
+            J = approximate_jacobian(fun,x_next);      
+        end
     end
+
+%Loop through until x is small
+    if use_analytical_jacobian == false
+    [f_val, J] = fun(x_guess);
+    x_next = x_guess;
+        while abs(J\f_val)> A_thresh && abs(f_val)>B_thresh && abs(det(J*J')) > 1e-14
+            %calculate next x
+            x_next = x_next - J\f_val;
+            %establish next y value
+            [f_val, J]  = fun(x_next);        
+        end
+    end
+
 end
