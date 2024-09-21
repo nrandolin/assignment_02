047880173b8d82623d0223a62ca96fad8a345e35
Ethan C <36670680+etbikehome@users.noreply.github.com>
2024-09-21 14:00:06 -0400

    It works!

--- approximate_jacobian.m
+++ approximate_jacobian.m
@@ -1,14 +1,11 @@
 function J = approximate_jacobian(fun,x)
-    ej = zeros(length(fun(x)), 1)
+    ej = zeros(length(x), 1);
     h = 1e-6;
-    size(fun(x))
-    size(x)
-    J = zeros(length(fun(x)), length(x))
-    for i = 1:length(ej)
+    J = zeros(length(fun(x)), length(x));
+    for i = 1:size(J, 2)
         ej(i) = 1;
-        partial = (fun(x+h.*ej) - fun(x-h.*ej))/(2.*h)
+        %partial = (fun(x+h*ej) - fun(x-h*ej))/(2*h)
         J(:, i) = (fun(x+h*ej) - fun(x-h*ej))/(2*h);
         ej(i) = 0;
-    end
-       
+    end 
 end
