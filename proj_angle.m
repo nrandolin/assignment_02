041f061f240baa321f9d1ed06493904f063969a0
lilydao11 <ldao@olin.edu>
2024-09-22 20:45:48 -0400

    mess around with finding projectile angle

--- proj_angle.m
+++ proj_angle.m
@@ -1,3 +1,28 @@
-x_val = multi_newton_solver(@target_traj, pi/3, true)
-V_p = projectile_traj(x_val, 6)
-run_simulation(x_val,20)
+%% TEST
+
+% wrapper
+theta_x = traj(pi/3)
+run_simulation(theta_x,6)
+
+function x_point = wrapper_x(t)
+    V_t = target_traj(t);
+    x_point = V_t(1);
+end
+
+function y_point = wrapper_y(t)
+    V_t = target_traj(t);
+    y_point = V_t(2);
+end
+
+function [theta_x, theta_y] = traj(theta_guess)
+    V_p = @(t) projectile_traj(t, theta_guess);
+    wrapper_x_2 = @(t) wrapper_x(t) - projectile_traj(t, theta_guess);
+    wrapper_y_2 = @(t) wrapper_y(t) - V_p(2);
+
+    theta_x = multi_newton_solver(wrapper_x_2, theta_guess, true);
+    theta_y = multi_newton_solver(wrapper_y_2, theta_guess, true);
+end
+
+% x_val = multi_newton_solver(@target_traj, pi/3, true)
+% V_p = projectile_traj(x_val, 6)
+% run_simulation(x_val,6)
--- test_numerical_jacobian.m
+++ test_numerical_jacobian.m
@@ -1,37 +1,37 @@
 %function to test numerical differentiation function
 function test_numerical_jacobian()
-%number of tests to perform
-num_tests = 100;
-%iterate num_tests times
+    %number of tests to perform
+    num_tests = 100;
+    %iterate num_tests times
     for n = 1:num_tests
-    %generate a randomized input and output dimension
-    input_dim = randi([1,15]);
-    output_dim = randi([1,15]);
-    %generate a input_dim x input_dim x output_dim matrix stack A
-    A = randn(input_dim,input_dim,output_dim);
-    %generate a matrix, B of dimension output_dim x input_dim
-    B = randn(output_dim,input_dim);
-    %generate a column vector, C of height output_dim
-    C = randn(output_dim,1);
-    %create a new test function
-    %this is essentially a random second-order (quadratic) function
-    %with input dimension input_dim and output dimension output_dim
-    test_fun = @(X) jacobian_test_function(X,A,B,C);
-    X_guess = randn(input_dim,1);
-    %evaluate numerical Jacobian of test_fun
-    %use whatever your function name was here!
-    J_numerical = approximate_jacobian(test_fun,X_guess);
-    %compute the analytical jacobian of jacobian_test_function
-    J_analytical = B;
+        %generate a randomized input and output dimension
+        input_dim = randi([1,15]);
+        output_dim = randi([1,15]);
+        %generate a input_dim x input_dim x output_dim matrix stack A
+        A = randn(input_dim,input_dim,output_dim);
+        %generate a matrix, B of dimension output_dim x input_dim
+        B = randn(output_dim,input_dim);
+        %generate a column vector, C of height output_dim
+        C = randn(output_dim,1);
+        %create a new test function
+        %this is essentially a random second-order (quadratic) function
+        %with input dimension input_dim and output dimension output_dim
+        test_fun = @(X) jacobian_test_function(X,A,B,C);
+        X_guess = randn(input_dim,1);
+        %evaluate numerical Jacobian of test_fun
+        %use whatever your function name was here!
+        J_numerical = approximate_jacobian(test_fun,X_guess);
+        %compute the analytical jacobian of jacobian_test_function
+        J_analytical = B;
         for n = 1:output_dim
-        J_analytical(n,:)=J_analytical(n,:)+X_guess'*A(:,:,n);
-        J_analytical(n,:)=J_analytical(n,:)+X_guess'*A(:,:,n)';
+            J_analytical(n,:)=J_analytical(n,:)+X_guess'*A(:,:,n);
+            J_analytical(n,:)=J_analytical(n,:)+X_guess'*A(:,:,n)';
         end
-    %compare with Jacobian of A
-    largest_error = max(max(abs(J_numerical-J_analytical)));
-    %if J is not close to A, print fail.
+        %compare with Jacobian of A
+        largest_error = max(max(abs(J_numerical-J_analytical)));
+        %if J is not close to A, print fail.
         if largest_error>1e-7
-        disp('fail!');
+            disp('fail!');
         end
     end
 end
@@ -39,7 +39,8 @@ end
 function f_val = jacobian_test_function(X,A,B,C)
 output_length = length(C);
 f_val = B*X+C;
-    for n = 1:output_length
-    f_val(n)=f_val(n)+(X'*A(:,:,n)*X);
-    end
+for n = 1:output_length
+f_val(n)=f_val(n)+(X'*A(:,:,n)*X);
 end
+end
+
